dfc7e5b55c5b1a54f5e85b0ed8fbf4dc3d00d158
glenntyson <gatyson261@gmail.com>
2026-02-09 07:12:16 -0500

    Made customer id and put in when a customer is picked

--- src/main/java/org/codedifferently/nameId.java
+++ src/main/java/org/codedifferently/nameId.java
@@ -1,8 +1,28 @@
 package org.codedifferently;
 
 
+import java.util.Random;
 
 public class nameId {
 
+    public static String code(String name, int id) {
+        String personName = name.trim().toUpperCase();
+        return personName.charAt(0) + personName.substring(personName.length() - 1) + id;
+
+    }
+public static String tag(Random random){
+        int pick = random.nextInt(4);
+        if (pick==0){
+            return "Did you win?";
+        } else if (pick==1){
+            return "Better luck next time!";
+        }
+        else if (pick==2){
+            return "You are a winner!";
+        }
+        else {
+            return "Try again!";
+        }
+}
 
 }
